006fb3c463c56717b3c55495d87f67803f225fc4
Cynthia <cynthiaselee@gmail.com>
2021-03-04 18:43:08 -0800

    To loop through and obtain length of each element in the array

---
+++ map_builtin/mapTest.js
@@ -0,0 +1,16 @@
+const lighthouses = ["Gibraltar Point", "Peggy's Point", "Cove Island", "Discovery Island", "Cape Scott", "Point Clark", "Kincardine"];
+
+const map = lighthouses.map(x => x.length);
+
+console.log(map)
+// write code here to use map
+// It should output: [ 15, 13, 11, 16, 10, 11, 10 ]
+
+
+/*const array1 = [1, 4, 9, 16];
+
+// pass a function to map
+const map1 = array1.map(x => x * 2);
+
+console.log(map1);
+// expected output: Array [2, 8, 18, 32]*/
